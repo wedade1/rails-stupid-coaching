e348cd34b07e909b49a10dea4d7a43150dd8c04b
Wedade Jerjoub <wedadejerjoub@gmail.com>
2024-08-12 14:44:30 +0100

    2nd done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,10 @@
 class QuestionsController < ApplicationController
+  def ask
+  end
+  def answer
+    @question = params[:question]
+    @answer = "I don't care, get dressed and go to work!"
+    @answer = "Great!" if @question == "I am going to work"
+    @answer = "Silly question, get dressed and go to work!" if @question == "I am going to work?"
+  end
 end
